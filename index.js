02985284b2ebbb1842ada6cae1ecfbce15f191fb
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-07-08 09:48:45 -0400

    submission

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if (ride <= '400ft'){
+    return 'ride is free'
+  }else if (ride < '400ft' && ride >= '2000ft'){
+  return 'ride is $20'
+  } else if (ride < '2000ft'){
+    return 'ride is $30'
+  } else return 'You cannot ride'
+
+
 }
 
 function ternaryCheckCity(){
